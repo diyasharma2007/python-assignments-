87269d468502eb292ce57c710c6cb88e11066927
diyasharma2007 <diyaasharmaa2007@gmail.com>
2026-04-19 01:34:35 +0530

    Add files via upload

---
+++ FileHandling(json file).py
@@ -0,0 +1,7 @@
+[
+    {"name": "diya", "mark": "90", "age": 20},
+    {"name": "riya", "mark": "90", "age": 20},
+    {"name": "tiya", "mark": "90", "age": 20},
+    {"name": "priya", "mark": "90", "age": 20}
+
+]
